a4591e81439ba32e151e349e79470d57b2b68ccf
user <user@WIN-4KGTNI4DDSK>
2025-09-21 09:43:42 +0300

    Add project files.

--- Pointer/home_work.cpp
+++ Pointer/home_work.cpp
@@ -45,3 +45,45 @@ int main()
     std::cout << std::endl;
 }
 #endif //TASK_2
+
+#ifdef TASK_3
+void reverseCopy(int* source, int* dest, int size) 
+{
+    int* srcPtr = source;// Указатель на начало исходного массива
+    int* destPtr = dest + size - 1;// Указатель на конец целевого массива
+
+    for (int i = 0; i < size; ++i)
+    {
+        *destPtr = *srcPtr; // Копируем элемент из source в dest в обратном порядке
+        ++srcPtr;           // Продвигаем указатель source вперед
+        --destPtr;          // Продвигаем указатель dest назад
+    }
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "");
+    int source[] = { 1, 2, 3, 4, 5 };
+    int size = sizeof(source) / sizeof(source[0]);
+
+    int* dest = new int[size]; // Динамическое выделение памяти
+
+    reverseCopy(source, dest, size);
+
+    std::cout << "Исходный массив: ";
+    for (int i = 0; i < size; ++i) 
+    {
+        std::cout << source[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Копия в обратном порядке: ";
+    for (int i = 0; i < size; ++i) 
+    {
+        std::cout << dest[i] << " ";
+    }
+    std::cout << std::endl;
+
+    delete[] dest; // Освобождение памяти
+}
+#endif //TASK_3
