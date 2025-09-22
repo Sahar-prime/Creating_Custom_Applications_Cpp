bc9c613e0ba769be88f6e7fbdfd243f43cd97ddb
user <user@WIN-4KGTNI4DDSK>
2025-09-22 20:52:30 +0300

    Add project files.

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -35,13 +35,25 @@ int main()
 	int newnumber;
 	std::cout << "Введите добавляемое число: ";
 	std::cin >> newnumber;
-	int* arr2 = new int[size + 1];
+	int pos;
+	std::cout << "Куда добавляем элемент: ";
+	std::cin >> pos;
+	int* arr2 = new int[size+1];
 	for (int i = 0; i < size; i++)
 	{
-		arr2[i] = arr[i];
+		/*(i < pos) ? arr2[i] = arr[i]: arr2[i + 1] = arr[i];
+		if (i < pos) 
+		{
+			arr2[i] = arr[i];
+		}
+		else 
+		{
+			arr2[i + 1] = arr[i];
+		}*/
+		arr2[i + (i >= pos)] = arr[i];
 	}
 	delete[]arr;
-	arr2[size] = newnumber;
+	arr2[pos] = newnumber;
 	size++;
 	arr = arr2;
 	arr2 = nullptr;
@@ -51,16 +63,77 @@ int main()
 	}
 	std::cout << std::endl;
 
+	int size_main, size_insert;
+	std::cout << "Введи размер основого массива: ";
+	std::cin >> size_main;
+	int* arr_main = new int[size_main];
+	for (int i = 0; i < size_main; i++)
+	{
+		arr_main[i] = rand() % 10;
+	}
+	std::cout << "Введи размер вставляемого массива: ";
+	std::cin >> size_insert;
+	int* arr_insert = new int[size_insert];
+	for (int i = 0; i < size_insert; i++)
+	{
+		arr_insert[i] = rand() % 10;
+	}
+	std::cout << std::endl;
+	std::cout << "Вставляемый массив:\n";
+	for (int i = 0; i < size_insert; i++)
+	{
+		std::cout << arr_insert[i] << " ";
+	}
+	std::cout << std::endl;
+	int pos3;
+	std::cout << "Введите позицию для ставки: ";
+	std::cin >> pos3;
+	int* new_arr = new int[size_main + size_insert];
+	for (int i = 0; i < size_main; i++)
+	{
+		if (i < pos3)
+		{
+			new_arr[i] = arr_main[i];
+		}
+		else
+		{
+			new_arr[i + size_insert] = arr_main[i];
+		}
+	}
+	for (int i = 0; i < size_insert; i++)
+	{
+		new_arr[i + pos3] = arr_insert[i];
+	}
+	delete[]arr_main;
+	size_main += size_insert;
+	arr_main = new_arr;
+	new_arr = nullptr;
+	for (int i = 0; i < size_main; i++)
+	{
+		std::cout << arr_main[i] << " ";
+	}
+	std::cout << std::endl;
+
 	//удаление числа из массива
+	int pos2;
+	std::cout << "Откуда удаляем элемент: ";
+	std::cin >> pos2;
 	int* arr3 = new int[size - 1];
 	for (int i = 0; i < size-1; i++) 
 	{
-		arr3[i] = arr[i];
+		if (i != pos2)
+		{
+			arr3[i] = arr[i];
+		}
+		else
+		{
+			arr3[i] = arr[i + 1];
+		}
 	}
 	delete[]arr;
+	size--;
 	arr = arr3;
 	arr3 = nullptr;
-	size--;
 	for (int i = 0; i < size; i++)
 	{
 		std::cout << arr[i] << " ";
