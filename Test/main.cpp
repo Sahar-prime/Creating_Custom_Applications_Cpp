da80c110ef7dd24915f0ec01274dbba95a80f92c
user <user@WIN-4KGTNI4DDSK>
2025-10-20 18:53:02 +0300

    Add project files.

---
+++ Test/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+//#define MAIN
+
+using namespace std;
+
+//Структура для бритвы (2 характеристики)
+struct Britva
+{
+    char proizvoditel[50];
+    char model[50];
+};
+
+//Структура для геля для бритья (2 характеристики)
+struct GelDlyaBritya
+{
+    char marka[50];
+    char tip[50];
+};
+
+//Структура для подарочного набора
+struct PodarochnyiNabor
+{
+    char nazvanie[100];
+    Britva britva;
+    GelDlyaBritya gel;
+};
+
+void init(PodarochnyiNabor& nabor)
+{
+    //Ввод данных для бритвы
+    cout << "Введите название подарочного набора: ";
+    cin.getline(nabor.nazvanie, 100);
+    cout << "--- Данные для бритвы ---" << endl;
+    cout << "Производитель: ";
+    cin.getline(nabor.britva.proizvoditel, 50);
+    cout << "Модель: ";
+    cin.getline(nabor.britva.model, 50);
+
+    //Ввод данных для геля
+    cout << "--- Данные для геля для бритья ---" << endl;
+    cout << "Марка: ";
+    cin.getline(nabor.gel.marka, 50);
+    cout << "Тип (например, для чувствительной кожи): ";
+    cin.getline(nabor.gel.tip, 50);
+}
+
+void printed_information(const PodarochnyiNabor& nabor)
+{
+    cout << endl << "--- Содержимое подарочного набора ---" << endl;
+    cout << "Название набора: " << nabor.nazvanie << endl;
+    cout << "Бритва:" << endl;
+    cout << "  Производитель: " << nabor.britva.proizvoditel << endl;
+    cout << "  Модель: " << nabor.britva.model << endl;
+    cout << "Гель для бритья:" << endl;
+    cout << "  Марка: " << nabor.gel.marka << endl;
+    cout << "  Тип: " << nabor.gel.tip << endl;
+}
+
+#ifdef MAIN
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    PodarochnyiNabor product;
+    init(product);
+    printed_information(product);
+}
+#endif //MAIN
+
+/*
+Создать структуру бритва (2 характеристики на выбор)
+и гель для бритья (2 характеристики на выбор).
+Собрать подарочный набор с доп. полем название.
+*/
